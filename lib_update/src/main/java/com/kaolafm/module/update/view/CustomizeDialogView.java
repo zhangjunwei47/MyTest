a121068d914c2e874cc749f93c912d7e55eed09e
zhangchao <zhangchao1@kaolafm.com>
2019-11-29 15:01:29 +0800

    ui完善

---
+++ lib_update/src/main/java/com/kaolafm/module/update/view/CustomizeDialogView.java
@@ -0,0 +1,34 @@
+package com.kaolafm.module.update.view;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kaolafm.module.update.R;
+
+public class CustomizeDialogView extends DialogFragment {
+
+    private View mRootView;
+
+    public static CustomizeDialogView getInstance() {
+        CustomizeDialogView customizeDialogView = new CustomizeDialogView();
+        return customizeDialogView;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        if (mRootView == null) {
+            mRootView = inflater.inflate(R.layout.customize_dialog, container, false);
+        }
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+
+
+
+}
