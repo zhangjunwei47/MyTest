3d033a5ce710544104bbc266220bb7b0d653bbde
zhangchao <zhangchao1@kaolafm.com>
2020-01-30 17:38:41 +0800

    添加测试

--- app/src/main/java/com/example/kaola/myapplication/rxjava/observable/Callee.java
+++ app/src/main/java/com/example/kaola/myapplication/rxjava/observable/Callee.java
@@ -2,10 +2,17 @@ package com.example.kaola.myapplication.rxjava.observable;
 
 /**
  * @ClassName Callee
- * @Description TODO
+ * @Description 对应 Observer 接电话的人
  * @Author zhangchao
  * @Date 2020-01-30 17:31
  * @Version 1.0
  */
-public class Callee {
+public interface Callee<T> {
+    void onCall(Release release);
+
+    void onReceive(T t);
+
+    void onCompleted();
+
+    void onError(Throwable t);
 }
